5bad71f1de6f47ad0262dae19cf91709f63f80f4
khairovayuliana <y.khayrova@inbox.ru>
2025-06-08 00:35:20 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,62 +3,62 @@
 #include "magicbook.h"
 #include <string>
 
-SECTION("Field types") {
-        static_assert(std::is_same_v<decltype(MagicBook::title), std::string>, 
-            "title must be of type std::string");
-        static_assert(std::is_same_v<decltype(MagicBook::year), int>, 
-            "year must be of type int");
-        static_assert(std::is_same_v<decltype(MagicBook::pages), int>, 
-            "pages must be of type int");
+static_assert(std::is_same_v<decltype(MagicBook::title), std::string>, 
+        "title must be of type std::string");
+static_assert(std::is_same_v<decltype(MagicBook::year), int>, 
+        "year must be of type int");
+static_assert(std::is_same_v<decltype(MagicBook::pages), int>, 
+        "pages must be of type int");
+    
+   
+TEST_CASE("Class initialization and assignment") {
+    MagicBook book;
+    
+    SECTION("Default values") { // check your initialization!
+        REQUIRE(book.title == ""); 
+        REQUIRE(book.year == 0);
+        REQUIRE(book.pages == 0);
     }
 
-TEST_CASE("Class definition and initialization") {
-    SECTION("Default initialization") {
-        MagicBook book;
-        REQUIRE(book.title == ""); // Check if title is initialized to ""
-        REQUIRE(book.year == 0); // Check if year is initialized to 0
-        REQUIRE(book.pages == 0); // Check if pages is initialized to 0
-    }
-
-    SECTION("Field assignment and access") {
-        MagicBook book;
-        book.title = "Advanced Spellcasting";
-        book.year = 1492;
-        book.pages = 742;
+    SECTION("Field assignment") {
+        book.title = "Ancient Spells Compendium";
+        book.year = 1500;
+        book.pages = 1200;
         
-        REQUIRE(book.title == "Advanced Spellcasting");
-        REQUIRE(book.year == 1492);
-        REQUIRE(book.pages == 742);
+        REQUIRE(book.title == "Ancient Spells Compendium");
+        REQUIRE(book.year == 1500);
+        REQUIRE(book.pages == 1200);
     }
+}
 
 TEST_CASE("Book size evaluation") {
     MagicBook book;
     
-    SECTION("Class 1 books") {
+    SECTION("LARGE books") {
         book.pages = 1000;
-        REQUIRE(evaluateBookSize(book) == 1);
+        REQUIRE(evaluateBookSize(book) == MagicBook::BookClass::LARGE);
         book.pages = 1500;
-        REQUIRE(evaluateBookSize(book) == 1);
+        REQUIRE(evaluateBookSize(book) == MagicBook::BookClass::LARGE);
         book.pages = 2000;
-        REQUIRE(evaluateBookSize(book) == 1);
+        REQUIRE(evaluateBookSize(book) == MagicBook::BookClass::LARGE);
     }
     
-    SECTION("Class 2 books") {
+    SECTION("MEDIUM books") {
         book.pages = 500;
-        REQUIRE(evaluateBookSize(book) == 2);
+        REQUIRE(evaluateBookSize(book) == MagicBook::BookClass::MEDIUM);
         book.pages = 750;
-        REQUIRE(evaluateBookSize(book) == 2);
+        REQUIRE(evaluateBookSize(book) == MagicBook::BookClass::MEDIUM);
         book.pages = 999;
-        REQUIRE(evaluateBookSize(book) == 2);
+        REQUIRE(evaluateBookSize(book) == MagicBook::BookClass::MEDIUM);
     }
     
-    SECTION("Class 3 books") {
+    SECTION("SMALL books") {
         book.pages = 1;
-        REQUIRE(evaluateBookSize(book) == 3);
+        REQUIRE(evaluateBookSize(book) == MagicBook::BookClass::SMALL);
         book.pages = 499;
-        REQUIRE(evaluateBookSize(book) == 3);
+        REQUIRE(evaluateBookSize(book) == MagicBook::BookClass::SMALL);
         book.pages = 250;
-        REQUIRE(evaluateBookSize(book) == 3);
+        REQUIRE(evaluateBookSize(book) == MagicBook::BookClass::SMALL);
     }
 }
 
@@ -86,4 +86,4 @@ TEST_CASE("Arcane year detection") {
         book.year = 2025;
         REQUIRE_FALSE(isFromArcaneYear(book));
     }
-    
+}
